97b07343988eb5b69e4187caf7b846f8dc3d5f5e
andreas <benzan81@gmail.com>
2022-03-16 19:01:25 +1300

    artpieces as react components

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Art01 from './Art01'
+import Art02 from './Art02'
+import Art03 from './Art03'
+import Art04 from './Art04'
 
 function App () {  
   return (
-    <h1>React development has begun!</h1>
+    <>
+    <h1>Art Gallery</h1>
+  
+    <Art04 />
+    <Art01 />
+    <Art02 />
+    <Art03 />
+
+  </>
   )
 }
 
